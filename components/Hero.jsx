058767afedebe09a52d666c9d289c3a29f881f09
mozmmil-ali <mly88207@gmail.com>
2024-05-21 12:14:37 +0200

    -

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,15 +1,19 @@
-"use client";
-
-import React from "react";
-
-import CustomButton from "./CustomButton";
-import Image from "next/image";
-
 const Hero = () => {
-  const handelScroll = () => {};
   return (
-    <div className="">
-      hero
+    <div className="masthead max-h-[100vh] h-screen">
+      <div className="h-full">
+        <div className="flex flex-col items-center justify-center h-full">
+          <h1 className="md:text-[72px] text-[48px] text-white font-bold">
+            Your Favorite Place for <br />
+            Free Bootstrap Themes
+          </h1>
+          <span></span>
+          <p>
+            Start Bootstrap can help you build better websites using the Bootstrap framework! Just download a theme and
+            start customizing, no strings attached!
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,10 +2,10 @@ import { headerLinks } from "@/constants";
 
 const Navbar = () => {
   return (
-    <header className="section">
-      <nav className="flex justify-between py-5">
+    <header className="absolute w-full">
+      <nav className="section flex justify-between py-5 md:flex-row flex-col">
         <div className="">
-          <a href="/" className="font-bold text-2xl">
+          <a href="/" className="font-bold text-2xl text-gray-300 hover:text-gray-200">
             Start Bootstrap
           </a>
         </div>
@@ -13,7 +13,7 @@ const Navbar = () => {
         <ul className="flex gap-8">
           {headerLinks.map((link) => (
             <li key={link.title}>
-              <a href={link.url} className="font-bold text-xl">
+              <a href={link.url} className="font-bold text-xl text-gray-300 hover:text-gray-200">
                 {link.title}
               </a>
             </li>
