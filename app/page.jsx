3b7e28c3ed395f7315e8dbaa9716679c9c838f7d
mozmmil-ali <mly88207@gmail.com>
2024-05-23 12:47:44 +0200

    -

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-import { About, CustomButton, Hero, Services } from "@/components";
+import { About, CustomButton, Footer, Hero, Services } from "@/components";
 
 export default function Home() {
   return (
@@ -18,6 +18,7 @@ export default function Home() {
           />
         </div>
       </section>
+      <Footer />
     </main>
   );
 }
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,18 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-100 text-center py-14 mt-36">
-      <p className="text-gray-600">Copyright © 2023 - Company Name</p>
+    <footer className="text-center py-28">
+      <div className="flex justify-center gap-4">
+        <span className="w-16 h-16 rounded-full flex items-center justify-center bg-primary-rgb  text-white text-3xl font-semibold">
+          <i className="icon-social-facebook"></i>
+        </span>
+        <span className="w-16 h-16 rounded-full flex items-center justify-center bg-primary-rgb  text-white text-3xl font-semibold">
+          <i className="icon-social-twitter"></i>
+        </span>
+        <span className="w-16 h-16 rounded-full flex items-center justify-center bg-primary-rgb  text-white text-3xl font-semibold">
+          <i className="icon-social-github"></i>
+        </span>
+      </div>
+      <p className="text-gray-600 mt-16">Copyright © 2023 - Company Name</p>
     </footer>
   );
 };
