4486988e75213c1a542301f9e96ddec07efd8104
mozmmil-ali <mly88207@gmail.com>
2024-05-23 11:26:10 +0200

    -

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-import { About, Hero, Services } from "@/components";
+import { About, CustomButton, Hero, Services } from "@/components";
 
 export default function Home() {
   return (
@@ -6,6 +6,18 @@ export default function Home() {
       <Hero />
       <About />
       <Services />
+      <section className="masthead2 h-[90vh] flex justify-center items-center">
+        <div className="text-center">
+          <h2 className="text-gray-900 text-6xl font-bold ">
+            Welcome to <span className="italic">your</span> <br /> next website!
+          </h2>
+          <CustomButton
+            title="download now!"
+            containerStyles="bg-primary-rgb text-white mt-10 inline-block"
+            url="#"
+          />
+        </div>
+      </section>
     </main>
   );
 }
