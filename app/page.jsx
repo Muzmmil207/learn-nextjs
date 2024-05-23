d059ee9dcf0d2813a7dbe377093f5ce03c1cca6f
mozmmil-ali <mly88207@gmail.com>
2024-05-23 09:26:07 +0200

    -

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
+import { Hero } from "@/components";
+
 export default function Home() {
   return (
     <main>
-      <h1 className="font-black text-4xl">He</h1>
+      <Hero />
     </main>
   );
 }
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,24 +3,18 @@ import { CustomButton } from ".";
 const Hero = () => {
   return (
     <div className="masthead max-h-[100vh] h-screen">
-      <div className="h-full">
-        <div className="flex flex-col items-center justify-center h-full">
-          <h1 className="md:text-[72px] text-[48px] text-white font-bold">
-            Your Favorite Place for <br />
-            Free Bootstrap Themes
-          </h1>
-          <span className="bg-orangeRed h-1 w-16 my-7"></span>
-          <p className="max-w-[800px] w-full text-center text-white font-normal">
-            Start Bootstrap can help you build better websites using the
-            Bootstrap framework! Just download a theme and start customizing, no
-            strings attached!
-          </p>
-          <CustomButton
-            title="find out more"
-            containerStyles="bg-orangeRed text-white my-7"
-            url="#about"
-          />
-        </div>
+      <div className="flex flex-col items-center justify-center h-full">
+        <h1 className="md:text-[124px] text-[48px] text-gray-900         font-bold">
+          Stylish Portfolio
+        </h1>
+        <p className="text-gray-900 text-center text-4xl  italic font-semibold">
+          A Free Bootstrap Theme by Start Bootstrap
+        </p>
+        <CustomButton
+          title="find out more"
+          containerStyles="bg-primary-rgb text-white my-7"
+          url="#about"
+        />
       </div>
     </div>
   );
