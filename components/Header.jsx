3163a504a5fdb89606b96cc19d2e538b78ff1c31
mozmmil-ali <mly88207@gmail.com>
2024-05-29 10:03:13 +0200

    build Header

---
+++ components/Header.jsx
@@ -0,0 +1,26 @@
+import { Bell } from "lucide-react";
+import Image from "next/image";
+
+function Header() {
+  return (
+    <header className="flex items-center justify-between">
+      <div className="font-bold text-2xl md:text-4xl">deel.</div>
+      <div className="flex items-center gap-6">
+        <div className="bg-zinc-400 cursor-pointer p-[1px] rounded-full">
+          <Image
+            src="/assets/imgs/p1.jpg"
+            alt=""
+            width={30}
+            height={30}
+            className="rounded-full"
+          />
+        </div>
+        <div className="cursor-pointer">
+          <Bell size={30} fontWeight={"bold"} />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
